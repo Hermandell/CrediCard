52ad1f41544f446dd950b4c6047814f6608f0baa
Eleazar Salazar <pepeslzr117@gmail.com>
2023-06-08 00:36:06 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards-2';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
+
+
+const PaymentForm = () => {
+  const [state, setState] = useState({
+    number: '',
+    expiry: '',
+    cvc: '',
+    name: '',
+    focus: '',
+  });
+
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+    
+    setState((prev) => ({ ...prev, [name]: value }));
+  }
+
+  const handleInputFocus = (evt) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  }
+
+  return (
+    <div>
+      <Cards
+        number={state.number}
+        expiry={"💹  💹"}
+        cvc={state.cvc}
+        name={"Pedro Eleazar Uribe Salazar"}
+        focused={state.focus}
+      />
+      <br/>
+      <form>
+        <input
+          type="number"
+          name="number"
+          placeholder="Card Number"
+          value={state.number}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        
+      </form>
+    </div>
+  );
+}
+
+export default PaymentForm;
